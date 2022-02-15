792bc26c844aae4b5c095f8306681f4733211db9
Frank <frank.devilliers@yandex.com>
2022-02-15 20:00:01 +0200

    4

--- scriptTest.js
+++ scriptTest.js
@@ -7,14 +7,21 @@ let countHuman = 0;
 let countPc = 0;
 
 
+function rt(){
+    
+    if(countHuman > countPc){
+        const test = document.createElement('h4');
+        test.textContent = "You Win";
+        document.getElementById('test').appendChild(test);
 
-   let b = document.querySelector("#b1");
-   b.onclick = function(){userSelect = 1}
+    }else{
+        const test = document.createElement('h4');
+        test.textContent = "Computer Wins";
+         document.getElementById('test').appendChild(test);
+    }
+}
 
- 
-   
-   
-   b.addEventListener("click", () => {
+function chooseOne(){
     let random = Math.floor(Math.random() * 3)+1;
     console.log(random + " pc");
     if(random == 1){
@@ -37,13 +44,14 @@ let countPc = 0;
             }
 
             if(random == 1){ 
+           
             const img = document.createElement('img');
             img.src = "Rock.jpg";
             img.style.height = '300px';
             img.style.width = '300px';
             img.style.display = "Flex";
             img.style.alignContent = "center";
-            document.body.appendChild(img);
+            document.body.append(img);
             function remove(){
                 document.body.removeChild(img);
             }
@@ -60,9 +68,96 @@ let countPc = 0;
             img.style.alignContent = "center";
             document.body.appendChild(img);
                 function remove(){
-                    document.body.removeChild(image);
+                    document.body.removeChild(img);
                 }
-                setTimeout(remove, 10000);
+                setTimeout(remove, 1000);
+            }
+                
+               // let timeout;
+               
+
+            if(random == 3){ 
+                let timeout;
+                function scoreFunction(){
+                    timeout = setTimeout(alertFunc, 1500);
+                }
+               
+                const img = document.createElement("img");
+                img.src = "Scissors.jpg";
+                img.style.height = '300px';
+                img.style.width = '300px';
+                img.style.alignContent = "center";
+                
+                document.getElementById('test').appendChild(img);
+                
+            
+                function remove(){
+                    document.getElementById('test').removeChild(img);
+                }
+                setTimeout(remove, 1000);
+            }
+                
+        
+
+       
+        
+        console.log(countPc + " Pc count");
+        console.log(countHuman + " Count");
+
+
+}
+
+function chooseTwo(){
+
+    let random = Math.floor(Math.random() * 3)+1;
+    console.log(random + " pc");
+    if(random == 1){
+        compSelect = "rock";
+    }
+    if(random == 2){
+        compSelect = "paper";
+    }
+    if(random == 3){
+        compSelect = "scissors";
+    }
+
+ if(compSelect == "paper"){
+            
+}
+if(compSelect == "scissors"){
+      countPc++
+}
+if(compSelect == "rock"){
+        countHuman++
+
+        if(random == 1){ 
+       
+            const img = document.createElement('img');
+            img.src = "Rock.jpg";
+            img.style.height = '300px';
+            img.style.width = '300px';
+            img.style.display = "Flex";
+            img.style.alignContent = "center";
+            document.body.append(img);
+            function remove(){
+                document.body.removeChild(img);
+            }
+            setTimeout(remove, 1000);
+        }
+
+            if(random == 2){ 
+                
+                const img = document.createElement('img');
+            img.src = "Paper.jpg";
+            img.style.height = '300px';
+            img.style.width = '300px';
+            img.style.display = "Flex";
+            img.style.alignContent = "center";
+            document.body.appendChild(img);
+                function remove(){
+                    document.body.removeChild(img);
+                }
+                setTimeout(remove, 1000);
             }
                 
                // let timeout;
@@ -86,71 +181,134 @@ let countPc = 0;
                 }
                 setTimeout(remove, 1000);
             }
-                
-        
+        }
+}
 
+function chooseThree(){
+    let random = Math.floor(Math.random() * 3)+1;
+    console.log(random + " pc");
+    if(random == 1){
+        compSelect = "rock";
+    }
+    if(random == 2){
+        compSelect = "paper";
+    }
+    if(random == 3){
+        compSelect = "scissors";
+    }
+
+    if(compSelect == "scissors"){
+            
+    }
+    if(compSelect == "paper"){
+        countHuman++
+    }
+    if(compSelect == "rock"){
+            countHuman++
+            }
+
+
+
+            if(random == 1){ 
        
-        
-        console.log(countPc + " Pc count");
-        console.log(countHuman + " Count");});
-   
-   
+                const img = document.createElement('img');
+                img.src = "Rock.jpg";
+                img.style.height = '300px';
+                img.style.width = '300px';
+                img.style.display = "Flex";
+                img.style.alignContent = "center";
+                document.body.append(img);
+                function remove(){
+                    document.body.removeChild(img);
+                }
+                setTimeout(remove, 1000);
+            }
+    
+                if(random == 2){ 
+                    
+                    const img = document.createElement('img');
+                img.src = "Paper.jpg";
+                img.style.height = '300px';
+                img.style.width = '300px';
+                img.style.display = "Flex";
+                img.style.alignContent = "center";
+                document.body.appendChild(img);
+                    function remove(){
+                        document.body.removeChild(img);
+                    }
+                    setTimeout(remove, 1000);
+                }
+                    
+                   // let timeout;
+                   
+    
+                if(random == 3){ 
+                    let timeout;
+                    function scoreFunction(){
+                        timeout = setTimeout(alertFunc, 1500);
+                    }
+                    const img = document.createElement("img");
+                    img.src = "Scissors.jpg";
+                    img.style.height = '300px';
+                    img.style.width = '300px';
+                    img.style.alignContent = "center";
+                    
+                    document.body.appendChild(img);
+                
+                    function remove(){
+                        document.body.removeChild(img);
+                    }
+                    setTimeout(remove, 1000);
+                }
+            }
+                    
 
+        
+    
+
+                
+
+
+        
+
+
+
+   let b = document.querySelector("#b1");
+   b.onclick = function(){userSelect = 1}
+   b.addEventListener("click", () => {
+    
+    rt();
+    chooseOne();
+    });
+   
+   
     let a = document.querySelector("#b2");
     a.onclick = function(){userSelect = 2}
     a.addEventListener("click", () => {
     
-        let random = Math.floor(Math.random() * 3)+1;
-        console.log(random + " pc");
-        if(random == 1){
-            compSelect = "rock";
-        }
-        if(random == 2){
-            compSelect = "paper";
-        }
-        if(random == 3){
-            compSelect = "scissors";
-        }
-
-     if(compSelect == "paper"){
-                
-    }
-    if(compSelect == "scissors"){
-          countPc++
-    }
-    if(compSelect == "rock"){
-            countHuman++
-            };});
+     chooseTwo();
+    });
+        
     
 
     let c = document.querySelector("#b3");
     c.onclick = function(){userSelect = 3}
     c.addEventListener("click", () => {
-        let random = Math.floor(Math.random() * 3)+1;
-        console.log(random + " pc");
-        if(random == 1){
-            compSelect = "rock";
-        }
-        if(random == 2){
-            compSelect = "paper";
-        }
-        if(random == 3){
-            compSelect = "scissors";
-        }
+       
+     chooseThree();
+     });
+    }
 
-        if(compSelect == "scissors"){
-                
-        }
-        if(compSelect == "paper"){
-            countHuman++
-        }
-        if(compSelect == "rock"){
-                countHuman++
-                }
 
-        console.log(userSelect);});
 
-    //console.log(userSelect);
+
+
+
+
+            
+
+   
+            
 
   
-}
+
